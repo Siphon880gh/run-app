137d7f1b400205e344cbda262b58d2a78b93749b
Weng Fei Fung <weffung@ucdavis.edu>
2022-10-15 21:21:44 -0700

    Major: Global countup. Local countup WIP

--- assets/week.js
+++ assets/week.js
@@ -1,4 +1,18 @@
 window.isPlaying = true;
+window.elapsed = 0;
+window.poller = null;
+window.matrix1 = [];
+window.matrix2 = [];
+
+var utils = {
+    toHHMMSS: (secs) => {
+        if(secs>=86400) return "00:00:00"; // User must have came back after 24 hours.
+
+        var date = new Date(1970,0,1);
+        date.setSeconds(secs);
+        return date.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+    } // toHHMMSS
+}
 
 $(()=>{
 
@@ -19,5 +33,25 @@ $(()=>{
         countNumeral++;
     });
 
+    // Prepare matrix that keeps track which phase should play next from the global timer
+    let reducer = 0;
+    window.matrix1 = $(".phase-timemark .planned").map((i, el)=> {  
+        let currentVal = parseFloat(el.innerText); 
+        let multiplier = el.innerText.includes("m")?60: (el.innerText.includes("h")?360:1)
+        currentVal *= multiplier;
+        reducer += currentVal;
+        return reducer; 
+    }).toArray();
+    
+    window.matrix2 = $(".phase-timemark .planned").map((i, el)=> 0).toArray();
+    if(typeof window.matrix2[0] !== "undefined") window.matrix2[0] = 1;
+
     // Countup
+    window.poller = setInterval(()=>{
+        // Global
+        const newHHMMSS= utils.toHHMMSS(window.elapsed);
+        $(".global-timer").text(newHHMMSS);
+
+        window.elapsed++;
+    }, 1000)
 })
