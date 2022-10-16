7c8b37ac4cf0e93076ff23b959a7f84764ed443a
Weng Fei Fung <weffung@ucdavis.edu>
2022-10-15 19:34:46 -0700

    Test: Initial tests passed

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,14 @@
 function saveCheckmarks() {
     console.log("Called saveCheckmarks");
     function test() {
-        console.assert()
+        console.assert(
+            $(".programs input[type='checkbox']:checked").length==1, 
+            "FAILED: One checkmark is checked. RESULT: " + $(".programs input[type='checkbox']").length + " UNLESS you checked more than one."
+        )
+        console.info("Please verify: " + $(".programs input[type='checkbox']:checked").eq())
+
     }
+    test();
 }
 
 function updateLinethroughs() {
@@ -13,6 +19,13 @@ function updateLinethroughs() {
 }
 
 $(()=>{
+    function testSetup(functionName) {
+        if(functionName==="saveCheckmarks" || functionName==="updateLinethroughs") {
+            $(".programs input[type='checkbox']").attr("checked", false);
+        }
+    }
+    testSetup("saveCheckmarks");
+
     // UI UI: Clicking the week number also checks it off
     $(".programs li > span").on("click", (event) => {
         const cb = event.target.previousElementSibling;
