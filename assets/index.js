d3a6db3656188b2a160b45d90a8eb7b52b9d5430
Weng Fei Fung <weffung@ucdavis.edu>
2022-10-15 19:16:22 -0700

    Major: Opens to placeholder paths for each week run

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,7 @@
 $(()=>{
     // UI UI: Clicking the week number also checks it off
     $(".programs li > span").on("click", (event) => {
-        const cb = event.target.previousSibling;
+        const cb = event.target.previousElementSibling;
         cb.checked = !cb.checked;
     })
 });
