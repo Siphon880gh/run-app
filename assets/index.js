32549e6cc957fe33f19cba4bed43d91a2ab15132
Weng Fei Fung <weffung@ucdavis.edu>
2022-10-15 19:22:24 -0700

    Test: Preparing test structure

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,31 @@
+function saveCheckmarks() {
+    console.log("Called saveCheckmarks");
+    function test() {
+        console.assert()
+    }
+}
+
+function updateLinethroughs() {
+    console.log("Called updateLinethroughs");
+    function test() {
+        console.assert()
+    }
+}
+
 $(()=>{
     // UI UI: Clicking the week number also checks it off
     $(".programs li > span").on("click", (event) => {
         const cb = event.target.previousElementSibling;
         cb.checked = !cb.checked;
+        $(cb).trigger("change");
     })
+
+    $(".programs input[type='checkbox']").on("change", (event)=> { 
+        event.preventDefault();
+        event.stopPropagation();
+
+        saveCheckmarks();
+        updateLinethroughs();
+      });
+
 });
