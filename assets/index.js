2fdff111130054bfe4e6beecd4dde84ba69ea964
Weng Fei Fung <weffung@ucdavis.edu>
2022-10-15 19:39:57 -0700

    Major: Saves checkboxes between app loads

--- assets/index.js
+++ assets/index.js
@@ -1,30 +1,35 @@
-function saveCheckmarks() {
-    console.log("Called saveCheckmarks");
-    function test() {
-        console.assert(
-            $(".programs input[type='checkbox']:checked").length==1, 
-            "FAILED: One checkmark is checked. RESULT: " + $(".programs input[type='checkbox']").length + " UNLESS you checked more than one."
-        )
-        console.info("Please verify: " + $(".programs input[type='checkbox']:checked").eq())
+// Todo: Create a store to track state of checkboxes so you don't repeatedly query for checkboxes
 
-    }
-    test();
+function saveCheckmarks() {
+    // console.log("Called saveCheckmarks");
+    // function test() {
+    //     console.assert(
+    //         $(".programs input[type='checkbox']:checked").length==1, 
+    //         "FAILED: One checkmark is checked. RESULT: " + $(".programs input[type='checkbox']").length + " UNLESS you checked more than one."
+    //     )
+    //     console.info("Please verify: " + $(".programs input[type='checkbox']:checked").eq())
+
+    // }
+    // test();
+
+    const checkState = $(".programs input[type='checkbox']").map((i, el)=> $(el).prop("checked") ? 1 : 0 ).toArray();
+    localStorage.setItem("RunApp__checks", checkState);
 }
 
 function updateLinethroughs() {
-    console.log("Called updateLinethroughs");
-    function test() {
-        console.assert()
-    }
+    // console.log("Called updateLinethroughs");
+    // function test() {
+    //     console.assert()
+    // }
 }
 
 $(()=>{
-    function testSetup(functionName) {
-        if(functionName==="saveCheckmarks" || functionName==="updateLinethroughs") {
-            $(".programs input[type='checkbox']").attr("checked", false);
-        }
-    }
-    testSetup("saveCheckmarks");
+    // function testSetup(functionName) {
+    //     if(functionName==="saveCheckmarks" || functionName==="updateLinethroughs") {
+    //         $(".programs input[type='checkbox']").attr("checked", false);
+    //     }
+    // }
+    // testSetup("saveCheckmarks");
 
     // UI UI: Clicking the week number also checks it off
     $(".programs li > span").on("click", (event) => {
